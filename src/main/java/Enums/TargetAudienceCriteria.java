7f1ba8e6250c37a0bfdca0e66731634c672eb8a6
Rishav <99601803+rishav981@users.noreply.github.com>
2024-04-28 14:44:36 +0530

    adding models

---
+++ src/main/java/Enums/TargetAudienceCriteria.java
@@ -0,0 +1,8 @@
+package Enums;
+
+public enum TargetAudienceCriteria {
+
+    MALE,
+    FEMALE,
+    OTHER
+}
---
+++ src/main/java/com/example/demo/PromotionManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotionManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromotionManagementApplication.class, args);
+	}
+
+}
